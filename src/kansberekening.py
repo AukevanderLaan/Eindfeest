bf4c635c62908e58ea963ccc1def212fd773e488
AukevanderLaan <aukevanderlaan2002@gmail.com>
2023-12-07 12:33:58 +0100

    map opgeschoond en alvast paar functies geschreven

---
+++ src/kansberekening.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+list_chance = []
+for i in range(0,12):
+    list_chance[i]
+
+
+class kansberekening:
+    def __init__(self):
+        self.total_stock = 416
+        self.list_stock = []
+        for i in range(0,12):  
+            self.list_stock[i] = 32
+        
+    def kans_bj():
+
+
+    def total(self):
+        for i in range(0,12):
+            self.total_stock += self.list_stock[i]
+        return self.total_stock
+    
+    def reset(self):
+        for i in range(0,12):  
+            self.list_stock[i] = 32
