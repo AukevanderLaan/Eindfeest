dc0dc4fb60733baf78b11f5a4dbabf13ebe228a4
AukevanderLaan <aukevanderlaan2002@gmail.com>
2023-12-07 12:44:51 +0100

    paar functies gemaakt

--- src/kansberekening.py
+++ src/kansberekening.py
@@ -1,25 +1,28 @@
 import numpy as np
 
-list_chance = []
-for i in range(0,12):
-    list_chance[i]
-
 
 class kansberekening:
     def __init__(self):
         self.total_stock = 416
         self.list_stock = []
-        for i in range(0,12):  
+        self.list_chance = []
+        for i in range(0, 12):
             self.list_stock[i] = 32
-        
-    def kans_bj():
+            self.list_chance[i] = 1 / 13
 
-
-    def total(self):
-        for i in range(0,12):
+    def total_cards(self):
+        for i in range(0, 12):
             self.total_stock += self.list_stock[i]
         return self.total_stock
-    
+
+    def chance(self, value):
+        p = self.list_stock[value] / self.total_cards()
+        self.list_chance[value] = p
+        return p
+
     def reset(self):
-        for i in range(0,12):  
+        for i in range(0, 12):
             self.list_stock[i] = 32
+
+    def card_drawn(self, value):
+        self.list_stock[value] -= 1
