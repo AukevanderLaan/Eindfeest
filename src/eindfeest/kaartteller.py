1e21553791f416a1c3b2e1ca346d22009257819c
semoussoren <soussoren7@gmail.com>
2023-12-11 15:31:44 +0100

    .ui bestand van hoog/neutraal/laag naar 2-ace

--- src/eindfeest/kaartteller.py
+++ src/eindfeest/kaartteller.py
@@ -8,130 +8,214 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (
-    QCoreApplication,
-    QDate,
-    QDateTime,
-    QLocale,
-    QMetaObject,
-    QObject,
-    QPoint,
-    QRect,
-    QSize,
-    Qt,
-    QTime,
-    QUrl,
-)
-from PySide6.QtGui import (
-    QBrush,
-    QColor,
-    QConicalGradient,
-    QCursor,
-    QFont,
-    QFontDatabase,
-    QGradient,
-    QIcon,
-    QImage,
-    QKeySequence,
-    QLinearGradient,
-    QPainter,
-    QPalette,
-    QPixmap,
-    QRadialGradient,
-    QTransform,
-)
-from PySide6.QtWidgets import (
-    QApplication,
-    QHBoxLayout,
-    QMainWindow,
-    QMenuBar,
-    QPushButton,
-    QSizePolicy,
-    QStatusBar,
-    QTextEdit,
-    QVBoxLayout,
-    QWidget,
-)
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QTextEdit,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
-            MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(708, 493)
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(792, 525)
         self.centralwidget = QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
+        self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayoutWidget = QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(80, 10, 551, 431))
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(10, 10, 771, 451))
         self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.reset_button = QPushButton(self.verticalLayoutWidget)
+        self.reset_button.setObjectName(u"reset_button")
+
+        self.verticalLayout.addWidget(self.reset_button)
+
         self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.low_card_button = QPushButton(self.verticalLayoutWidget)
-        self.low_card_button.setObjectName("low_card_button")
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.card_button_2 = QPushButton(self.verticalLayoutWidget)
+        self.card_button_2.setObjectName(u"card_button_2")
 
-        self.horizontalLayout_2.addWidget(self.low_card_button)
+        self.horizontalLayout_2.addWidget(self.card_button_2)
 
-        self.neutral_card_button = QPushButton(self.verticalLayoutWidget)
-        self.neutral_card_button.setObjectName("neutral_card_button")
+        self.card_button_3 = QPushButton(self.verticalLayoutWidget)
+        self.card_button_3.setObjectName(u"card_button_3")
 
-        self.horizontalLayout_2.addWidget(self.neutral_card_button)
+        self.horizontalLayout_2.addWidget(self.card_button_3)
 
-        self.high_card_button = QPushButton(self.verticalLayoutWidget)
-        self.high_card_button.setObjectName("high_card_button")
+        self.card_button_4 = QPushButton(self.verticalLayoutWidget)
+        self.card_button_4.setObjectName(u"card_button_4")
 
-        self.horizontalLayout_2.addWidget(self.high_card_button)
+        self.horizontalLayout_2.addWidget(self.card_button_4)
+
+        self.card_button_5 = QPushButton(self.verticalLayoutWidget)
+        self.card_button_5.setObjectName(u"card_button_5")
+
+        self.horizontalLayout_2.addWidget(self.card_button_5)
+
+        self.card_button_6 = QPushButton(self.verticalLayoutWidget)
+        self.card_button_6.setObjectName(u"card_button_6")
+
+        self.horizontalLayout_2.addWidget(self.card_button_6)
+
+        self.card_button_7 = QPushButton(self.verticalLayoutWidget)
+        self.card_button_7.setObjectName(u"card_button_7")
+
+        self.horizontalLayout_2.addWidget(self.card_button_7)
+
+        self.card_button_8 = QPushButton(self.verticalLayoutWidget)
+        self.card_button_8.setObjectName(u"card_button_8")
+
+        self.horizontalLayout_2.addWidget(self.card_button_8)
+
 
         self.verticalLayout.addLayout(self.horizontalLayout_2)
 
         self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.text_low = QTextEdit(self.verticalLayoutWidget)
-        self.text_low.setObjectName("text_low")
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.text_2 = QTextEdit(self.verticalLayoutWidget)
+        self.text_2.setObjectName(u"text_2")
 
-        self.horizontalLayout.addWidget(self.text_low)
+        self.horizontalLayout.addWidget(self.text_2)
 
-        self.text_neutral = QTextEdit(self.verticalLayoutWidget)
-        self.text_neutral.setObjectName("text_neutral")
+        self.text_3 = QTextEdit(self.verticalLayoutWidget)
+        self.text_3.setObjectName(u"text_3")
 
-        self.horizontalLayout.addWidget(self.text_neutral)
+        self.horizontalLayout.addWidget(self.text_3)
 
-        self.text_high = QTextEdit(self.verticalLayoutWidget)
-        self.text_high.setObjectName("text_high")
+        self.text_4 = QTextEdit(self.verticalLayoutWidget)
+        self.text_4.setObjectName(u"text_4")
 
-        self.horizontalLayout.addWidget(self.text_high)
+        self.horizontalLayout.addWidget(self.text_4)
+
+        self.text_5 = QTextEdit(self.verticalLayoutWidget)
+        self.text_5.setObjectName(u"text_5")
+
+        self.horizontalLayout.addWidget(self.text_5)
+
+        self.text_6 = QTextEdit(self.verticalLayoutWidget)
+        self.text_6.setObjectName(u"text_6")
+
+        self.horizontalLayout.addWidget(self.text_6)
+
+        self.text_7 = QTextEdit(self.verticalLayoutWidget)
+        self.text_7.setObjectName(u"text_7")
+
+        self.horizontalLayout.addWidget(self.text_7)
+
+        self.text_8 = QTextEdit(self.verticalLayoutWidget)
+        self.text_8.setObjectName(u"text_8")
+
+        self.horizontalLayout.addWidget(self.text_8)
+
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.card_button_9 = QPushButton(self.verticalLayoutWidget)
+        self.card_button_9.setObjectName(u"card_button_9")
+
+        self.horizontalLayout_3.addWidget(self.card_button_9)
+
+        self.card_button_10 = QPushButton(self.verticalLayoutWidget)
+        self.card_button_10.setObjectName(u"card_button_10")
+
+        self.horizontalLayout_3.addWidget(self.card_button_10)
+
+        self.card_button_jack = QPushButton(self.verticalLayoutWidget)
+        self.card_button_jack.setObjectName(u"card_button_jack")
+
+        self.horizontalLayout_3.addWidget(self.card_button_jack)
+
+        self.card_button_queen = QPushButton(self.verticalLayoutWidget)
+        self.card_button_queen.setObjectName(u"card_button_queen")
+
+        self.horizontalLayout_3.addWidget(self.card_button_queen)
+
+        self.card_button_king = QPushButton(self.verticalLayoutWidget)
+        self.card_button_king.setObjectName(u"card_button_king")
+
+        self.horizontalLayout_3.addWidget(self.card_button_king)
+
+        self.card_button_11 = QPushButton(self.verticalLayoutWidget)
+        self.card_button_11.setObjectName(u"card_button_11")
+
+        self.horizontalLayout_3.addWidget(self.card_button_11)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.text_9 = QTextEdit(self.verticalLayoutWidget)
+        self.text_9.setObjectName(u"text_9")
+
+        self.horizontalLayout_4.addWidget(self.text_9)
+
+        self.text_10 = QTextEdit(self.verticalLayoutWidget)
+        self.text_10.setObjectName(u"text_10")
+
+        self.horizontalLayout_4.addWidget(self.text_10)
+
+        self.text_jack = QTextEdit(self.verticalLayoutWidget)
+        self.text_jack.setObjectName(u"text_jack")
+
+        self.horizontalLayout_4.addWidget(self.text_jack)
+
+        self.text_queen = QTextEdit(self.verticalLayoutWidget)
+        self.text_queen.setObjectName(u"text_queen")
+
+        self.horizontalLayout_4.addWidget(self.text_queen)
+
+        self.text_king = QTextEdit(self.verticalLayoutWidget)
+        self.text_king.setObjectName(u"text_king")
+
+        self.horizontalLayout_4.addWidget(self.text_king)
+
+        self.text_ace = QTextEdit(self.verticalLayoutWidget)
+        self.text_ace.setObjectName(u"text_ace")
+
+        self.horizontalLayout_4.addWidget(self.text_ace)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_4)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
-        self.menubar.setObjectName("menubar")
-        self.menubar.setGeometry(QRect(0, 0, 708, 22))
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 792, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
+        self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
 
         QMetaObject.connectSlotsByName(MainWindow)
-
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(
-            QCoreApplication.translate("MainWindow", "MainWindow", None)
-        )
-        self.low_card_button.setText(
-            QCoreApplication.translate("MainWindow", "Low card (2-6)", None)
-        )
-        self.neutral_card_button.setText(
-            QCoreApplication.translate("MainWindow", "Neutral card (7-9)", None)
-        )
-        self.high_card_button.setText(
-            QCoreApplication.translate("MainWindow", "High card (10-ace)", None)
-        )
-
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.reset_button.setText(QCoreApplication.translate("MainWindow", u"Reset", None))
+        self.card_button_2.setText(QCoreApplication.translate("MainWindow", u"2", None))
+        self.card_button_3.setText(QCoreApplication.translate("MainWindow", u"3", None))
+        self.card_button_4.setText(QCoreApplication.translate("MainWindow", u"4", None))
+        self.card_button_5.setText(QCoreApplication.translate("MainWindow", u"5", None))
+        self.card_button_6.setText(QCoreApplication.translate("MainWindow", u"6", None))
+        self.card_button_7.setText(QCoreApplication.translate("MainWindow", u"7", None))
+        self.card_button_8.setText(QCoreApplication.translate("MainWindow", u"8", None))
+        self.card_button_9.setText(QCoreApplication.translate("MainWindow", u"9", None))
+        self.card_button_10.setText(QCoreApplication.translate("MainWindow", u"10", None))
+        self.card_button_jack.setText(QCoreApplication.translate("MainWindow", u"Jack", None))
+        self.card_button_queen.setText(QCoreApplication.translate("MainWindow", u"Queen", None))
+        self.card_button_king.setText(QCoreApplication.translate("MainWindow", u"King", None))
+        self.card_button_11.setText(QCoreApplication.translate("MainWindow", u"ace", None))
     # retranslateUi
+
